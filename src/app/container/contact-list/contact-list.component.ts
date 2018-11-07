0d8d8a3e591d8eebb615606eb2adec63c320fe50
Tran Cong Nam <t.congnamvn@gmail.com>
2018-11-07 17:06:52 +0700

    use template to make a button and set a new message after 1s

--- src/app/container/contact-list/contact-list.component.ts
+++ src/app/container/contact-list/contact-list.component.ts
@@ -5,10 +5,19 @@ import { Component } from '@angular/core';
     selector: 'app-contact-list',
     template: `
     <h2>This is my contact list application: {{ message }}</h2>
+    <button (click)= "doClick()">Click me</button>
     `
 
 })
 
 export class ContactListComponent {
     message: string = ' Hi there';
+    doClick() {
+        this.message = 'Button is clicked';
+        setTimeout(() => {
+            this.message = ' you just wasted 1s';
+
+        }, 1000);
+
+    }
 }
